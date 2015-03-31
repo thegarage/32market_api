54e981b0d8f33167195c8536684414773ba80b44
Chris Nelson <christopher.dnelson@uhc.com>
2015-03-31 15:42:45 -0500

    Add rspec config

---
+++ spec/spec_helper.rb
@@ -0,0 +1,46 @@
+require 'bundler/setup'
+Bundler.setup
+
+require '32market_api'
+
+RSpec.configure do |config|
+  # add support for focused specs with focus: true option
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  # disable foo.should == bar syntax
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
+
+    mocks.verify_partial_doubles = true
+  end
+end
